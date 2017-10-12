268e8417d8cfce0f12ec60a67a7a7810adecb341
arodrigoca <adri_rodri_64@hotmail.es>
2017-10-12 13:19:40 +0200

    SMIL handler done

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -4,19 +4,27 @@
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
-tags = {
-    'root-layout': [],
-    'region': [],
-    'img': [],
-    'audio': [],
-    'textstream': []
-}
+tags = []
+#this are the lists of possible attributes for each tag
+regList = ["id", "top", "left", "bottom", "right"]
+rootList = ["width", "height", "background-color"]
+imgList = ["src", "region", "begin", "dur"]
+audList = ["src", "begin", "dur"]
+textList = ["src", "region", "fill"]
+
+def findAttValue(string, attrs):
+
+	value = attrs.get(string, "")
+	return value
+
+def findAttrs(dictionary, listing, attrs):
+
+	for attribute in listing:
+		dictionary[attribute] = findAttValue(attribute, attrs)
 
 
 class SmallSMILHandler(ContentHandler):
-    """
-    Class to handle bad puns
-    """
+
 
     def __init__ (self):
         """
@@ -25,20 +33,39 @@ class SmallSMILHandler(ContentHandler):
     def startElement(self, name, attrs):
 
         if name == 'root-layout':
-            """
-            tags['root-layout'].append(attrs.get('width', ""))
-            print(tags['root-layout'])
-            """
-            pass
-        elif name == 'region':
-            pass
-        elif name == 'img':
-            pass
-        elif name == 'audio':
-            pass
-        elif name == 'textstream':
-            pass
 
+        	rootDict = {'tag': name}
+        	findAttrs(rootDict, rootList, attrs) 
+        	tags.append(rootDict)
+
+        elif name == 'region':
+
+        	regDict = {'tag': name}
+        	findAttrs(regDict, regList, attrs)
+        	tags.append(regDict)
+
+        elif name == 'img':
+
+        	imgDict = {'tag': name}
+        	findAttrs(imgDict, imgList, attrs)
+        	tags.append(imgDict)
+
+        elif name == 'audio':
+            
+            audDict = {'tag': name}
+            findAttrs(audDict, audList, attrs)
+            tags.append(audDict)
+
+        elif name == 'textstream':
+
+        	textDict = {'tag': name}
+        	findAttrs(textDict, textList, attrs)
+        	tags.append(textDict)
+
+    def get_tags():
+
+    	print(tags)
+    	return tags
 
 if __name__ == "__main__":
     """
@@ -48,3 +75,4 @@ if __name__ == "__main__":
     cHandler = SmallSMILHandler()
     parser.setContentHandler(cHandler)
     parser.parse(open('karaoke.smil'))
+    print(tags)
