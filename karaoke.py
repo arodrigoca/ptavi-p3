688208e3dc144ad41cb2b6edaea6a32a8eb8502c
arodrigoca <adri_rodri_64@hotmail.es>
2017-10-13 13:34:54 +0200

    karaoke.py almost finished. JSON compatibility in TODO list

--- karaoke.py
+++ karaoke.py
@@ -4,13 +4,33 @@
 from smallsmilhandler import SmallSMILHandler
 import sys
 
+def makeTagLines(tags):
+
+	tagLine = ''
+	tagLines = []
+
+	for tag in tags:
+		tagLine = tag['tag']
+		for attribute in tag:
+			if attribute != 'tag' and tag[attribute] != '':
+				tagLine = tagLine + '\t' + attribute + '=' + tag[attribute]
+		tagLines.append(tagLine)
+
+	return tagLines
+
+def printTags(tags):
+
+	for element in makeTagLines(tags):
+		print(element)
+
+
 if __name__ == "__main__":
 
 	try:
 		file = sys.argv[1]
 		handler = SmallSMILHandler()
-		text = handler.get_tags(file)
-		print(len(text))
+		tags = handler.get_tags(file)
+		printTags(tags)
 
-	except FileNotFoundError:
+	except (FileNotFoundError, IndexError):
 		print("Usage: python3 karaoke.py file.smil")
