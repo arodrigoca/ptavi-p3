888f9b679ef00f8291c798817503d71493ebda90
arodrigoca <adri_rodri_64@hotmail.es>
2017-10-13 17:00:18 +0200

    json conversion added

--- karaoke.py
+++ karaoke.py
@@ -3,6 +3,7 @@
 
 from smallsmilhandler import SmallSMILHandler
 import sys
+import json
 
 def makeTagLines(tags):
 
@@ -23,6 +24,22 @@ def printTags(tags):
 	for element in makeTagLines(tags):
 		print(element)
 
+def splitFileName():
+
+	file = sys.argv[1].split(".")
+	file = file[0]
+
+	return file 
+
+def toJSON(tags, file):
+
+	fileName = splitFileName()
+	fileName = fileName + '.json'
+
+	with open(fileName, "w") as f:
+		json.dump(tags, f)
+
+
 
 if __name__ == "__main__":
 
@@ -31,6 +48,7 @@ if __name__ == "__main__":
 		handler = SmallSMILHandler()
 		tags = handler.get_tags(file)
 		printTags(tags)
+		toJSON(tags, file)
 
 	except (FileNotFoundError, IndexError):
 		print("Usage: python3 karaoke.py file.smil")
