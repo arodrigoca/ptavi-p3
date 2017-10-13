d41d5da42066c1c1ae583082662480aa86624738
arodrigoca <adri_rodri_64@hotmail.es>
2017-10-13 19:47:06 +0200

    more work done

--- karaoke.py
+++ karaoke.py
@@ -4,51 +4,96 @@
 from smallsmilhandler import SmallSMILHandler
 import sys
 import json
+from urllib.request import urlretrieve
+import os
+
 
 def makeTagLines(tags):
 
-	tagLine = ''
-	tagLines = []
+    tagLine = ''
+    tagLines = []
 
-	for tag in tags:
-		tagLine = tag['tag']
-		for attribute in tag:
-			if attribute != 'tag' and tag[attribute] != '':
-				tagLine = tagLine + '\t' + attribute + '=' + tag[attribute]
-		tagLines.append(tagLine)
+    for tag in tags:
+        tagLine = tag['tag']
+        for attribute in tag:
+            if attribute != 'tag' and tag[attribute] != '':
+                tagLine = tagLine + '\t' + attribute + '=' + tag[attribute]
+        tagLines.append(tagLine)
 
-	return tagLines
+    return tagLines
+
 
 def printTags(tags):
 
-	for element in makeTagLines(tags):
-		print(element)
+    for element in makeTagLines(tags):
+        print(element)
+
 
 def splitFileName():
 
-	file = sys.argv[1].split(".")
-	file = file[0]
+    file = sys.argv[1].split(".")
+    file = file[0]
 
-	return file 
+    return file
+
 
 def toJSON(tags, file):
 
-	fileName = splitFileName()
-	fileName = fileName + '.json'
+    fileName = splitFileName()
+    fileName = fileName + '.json'
 
-	with open(fileName, "w") as f:
-		json.dump(tags, f)
+    with open(fileName, "w") as f:
+        json.dump(tags, f)
 
+
+def isHTTP(source):
+
+    result = "http://" in source
+
+    return result
+
+def createFileName(source):
+
+    workingDirectory = os.getcwd()
+    nameStart = source.rfind("/") + 1
+    newName = source[nameStart:]
+    print(newName)
+
+def downloadSource(source):
+
+
+    try:
+        urlretrieve(source)
+        createFileName(source)        
+
+    except ValueError:
+        pass
+
+
+def getRemoteSources(tags):
+
+    for tag in tags:
+        try:
+            source = tag['src']
+            #print(source)
+            #print(isHTTP(source))
+            if isHTTP:
+                downloadSource(source)
+
+        except KeyError:
+            pass
 
 
 if __name__ == "__main__":
 
-	try:
-		file = sys.argv[1]
-		handler = SmallSMILHandler()
-		tags = handler.get_tags(file)
-		printTags(tags)
-		toJSON(tags, file)
+    try:
+        file = sys.argv[1]
+        handler = SmallSMILHandler()
+        tags = handler.get_tags(file)
+        printTags(tags)
+        toJSON(tags, file)
+        getRemoteSources(tags)
+        print(os.getcwd())
 
-	except (FileNotFoundError, IndexError):
-		print("Usage: python3 karaoke.py file.smil")
+    except(FileNotFoundError, IndexError):
+        print("Usage: python3 karaoke.py file.smil")
--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -5,70 +5,71 @@ from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
 tags = []
-#this are the lists of possible attributes for each tag
+##this are the lists of possible attributes for each tag
 regList = ["id", "top", "left", "bottom", "right"]
 rootList = ["width", "height", "background-color"]
 imgList = ["src", "region", "begin", "dur"]
 audList = ["src", "begin", "dur"]
 textList = ["src", "region", "fill"]
 
+
 def findAttValue(string, attrs):
 
-	value = attrs.get(string, "")
-	return value
+    value = attrs.get(string, "")
+    return value
+
 
 def findAttrs(dictionary, listing, attrs):
 
-	for attribute in listing:
-		dictionary[attribute] = findAttValue(attribute, attrs)
+    for attribute in listing:
+        dictionary[attribute] = findAttValue(attribute, attrs)
 
 
 class SmallSMILHandler(ContentHandler):
 
+    def __init__(self):
 
-    def __init__ (self):
-        """
-        """
+        pass
 
     def startElement(self, name, attrs):
 
         if name == 'root-layout':
 
-        	rootDict = {'tag': name}
-        	findAttrs(rootDict, rootList, attrs) 
-        	tags.append(rootDict)
+            rootDict = {'tag': name}
+            findAttrs(rootDict, rootList, attrs)
+            tags.append(rootDict)
 
         elif name == 'region':
 
-        	regDict = {'tag': name}
-        	findAttrs(regDict, regList, attrs)
-        	tags.append(regDict)
+            regDict = {'tag': name}
+            findAttrs(regDict, regList, attrs)
+            tags.append(regDict)
 
         elif name == 'img':
 
-        	imgDict = {'tag': name}
-        	findAttrs(imgDict, imgList, attrs)
-        	tags.append(imgDict)
+            imgDict = {'tag': name}
+            findAttrs(imgDict, imgList, attrs)
+            tags.append(imgDict)
 
         elif name == 'audio':
-            
+
             audDict = {'tag': name}
             findAttrs(audDict, audList, attrs)
             tags.append(audDict)
 
         elif name == 'textstream':
 
-        	textDict = {'tag': name}
-        	findAttrs(textDict, textList, attrs)
-        	tags.append(textDict)
+            textDict = {'tag': name}
+            findAttrs(textDict, textList, attrs)
+            tags.append(textDict)
 
     def get_tags(self, file):
 
-    	parser = make_parser()
-    	cHandler = SmallSMILHandler()
-    	parser.setContentHandler(cHandler)
-    	parser.parse(open(file))
-    	return tags
+        parser = make_parser()
+        cHandler = SmallSMILHandler()
+        parser.setContentHandler(cHandler)
+        parser.parse(open(file))
+        return tags
 
 if __name__ == "__main__":
     """
